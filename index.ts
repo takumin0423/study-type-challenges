9ab4202db8979c9e83179ff2b95c526c422892e0
takumin0423 <takumin0423baske@gmail.com>
2025-02-25 23:47:10 +0900

    feat: 🎸 Pick

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
 /* _____________ ここにコードを記入 _____________ */
 
-type HelloWorld = string;
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
 
 /* _____________ テストケース _____________ */
-import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
