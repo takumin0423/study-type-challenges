66facd4636f624acae9c9e2e85b6f00b27959b0e
takumin0423 <takumin0423baske@gmail.com>
2025-03-01 23:04:11 +0900

    feat: 🎸 Awaited

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 /* _____________ ここにコードを記入 _____________ */
 
-type MyExclude<T, U> = T extends U ? never : T;
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T;
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
 
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
+
 type cases = [
-  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
-  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
-  Expect<
-    Equal<MyExclude<string | number | (() => void), Function>, string | number>
-  >
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>
 ];
