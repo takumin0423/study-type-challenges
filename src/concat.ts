e6f3fa856aa66b44058aa6501c42b5b8b5134a01
takumin0423 <software.takumi.igusa@gmail.com>
2026-02-25 01:14:23 +0900

    Concat

---
+++ src/concat.ts
@@ -0,0 +1,27 @@
+/* _____________ ここにコードを記入 _____________ */
+
+type Concat<T extends readonly unknown[], U extends readonly unknown[]> = [
+	...T,
+	...U,
+];
+
+/* _____________ テストケース _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = [1] as const;
+
+type cases = [
+	Expect<Equal<Concat<[], []>, []>>,
+	Expect<Equal<Concat<[], [1]>, [1]>>,
+	Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+	Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+	Expect<
+		Equal<
+			Concat<["1", 2, "3"], [false, boolean, "4"]>,
+			["1", 2, "3", false, boolean, "4"]
+		>
+	>,
+];
+
+// @ts-expect-error
+type error = Concat<null, undefined>;
